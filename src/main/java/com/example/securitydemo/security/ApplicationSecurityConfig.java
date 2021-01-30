eb1c3f067ff94ea798b87fa9f4846c1091116c77
DaniilRudyak <45008863+DaniilRudyak@users.noreply.github.com>
2021-01-30 19:30:55 +0300

    add management controller

--- src/main/java/com/example/securitydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/securitydemo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ package com.example.securitydemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,7 +15,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.example.securitydemo.security.ApplicationUserRole.*;
-
+import static com.example.securitydemo.security.ApplicationUserPermission.*;
+/*
+* ПОРЯДОК antMatchers ОЧЕНЬ ВАЖЕН ТАК КАК ПРОИСХОДИТ ПЕРЕБОРКА СОВПАДЕНИЙ
+* НАПРИМЕР antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name()) ЕСЛИ Я
+* УБЕРУ ОПРЕДЕЛЕНИЕ МЕТОДА HTTTP antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())ТО
+* ПРИ КОМПИЛЯЦИИ МОЖНО ДАТЬ ПРАВА НЕ ТОМУ ПОЛЬЗОВАТЕЛЮ(в зависимости его расположения)
+*
+*
+* */
 @Configuration//конфигурация нужна там где определяются бины
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -34,6 +43,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers( "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -46,19 +59,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmith = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())// вот как видит spring security role: ROLE_STUDENT
+//                .roles(STUDENT.name())// вот как видит spring security role: ROLE_STUDENT
+                .authorities(STUDENT.getGrantesAuthorities())
                 .build();
 
         UserDetails linda = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())// вот как видит spring security role: ROLE_ADMIN
+//                .roles(ADMIN.name())// вот как видит spring security role: ROLE_ADMIN
+                .authorities(ADMIN.getGrantesAuthorities())
                 .build();
 
         UserDetails tom = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMINTRAINEE.name())// вот как видит spring security role: ROLE_ADMINTRAINEE
+//                .roles(ADMINTRAINEE.name())// вот как видит spring security role: ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantesAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
